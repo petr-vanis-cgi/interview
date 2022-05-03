315cf2f346985ddc5af02a2ee8eb62c23f8cbc66
Wertiumor <83375995+Wertiumor@users.noreply.github.com>
2022-05-03 03:36:06 +0200

    Update Main.java
    Nechávám i se svými komentáři, ať je o čem se ve čtvrtek bavit. :)

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -2,24 +2,32 @@ package com.cgi.boat.interview;
 
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Map<String, List<String>> firstByLast = PeopleProcessor.firstnamesByLastname(PeopleSetup.people);
-        Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
+	public static void main(String[] args) {
+		Map<String, List<String>> firstByLast = PeopleProcessor.firstnamesByLastname(PeopleSetup.people);
+		Map<String, List<String>> lastByFirst = PeopleProcessor.lastnamesByFirstname(PeopleSetup.people);
 
-        // TODO: Print out 3 most common first names along with number of occurrences
-        // for example:
-        // Homer: 32
-        // Bart: 21
-        // William: 3
-    }
+////	   System.out.println("OriginalForTest");
+////    	for(int i = 0; i < PeopleSetup.people.size(); i++) {
+////            System.out.println(PeopleSetup.people.get(i).getFirstName()+ " " + PeopleSetup.people.get(i).getLastName());
+////				}
+		System.out.println("NamesGroups");
+		firstByLast.forEach((k, v) -> System.out.println(k + "\t->\t" + v));
+		lastByFirst.forEach((k, v) -> System.out.println(k + "\t->\t" + v));
 
-
-
-
-
-
-
+////		System.out.println("Searchname");
+////		PeopleProcessor.searchName(firstByLast);
+		// TODO: Print out 3 most common first names along with number of occurrences
+		// for example:
+		// Homer: 32
+		// Bart: 21
+		// William: 3
+//		System.out.println("Largest occurrency of Name");
+//		PeopleProcessor.sortByValueSize(firstByLast)
+//				.forEach((k, v) -> System.out.println(k + ": " + v.size() + "\t->\t" + v));
+PeopleProcessor.sortByValueSizeTop3(firstByLast);
+	}
 }
