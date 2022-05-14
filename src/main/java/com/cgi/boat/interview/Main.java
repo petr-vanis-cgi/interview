ac06420f86453a1892b637d5955ca885eff7ba81
PeterSzepesi <peter.szepesi.ferenc@gmail.com>
2022-05-15 00:03:48 +0200

    Implemented interview problem.

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -2,6 +2,7 @@ package com.cgi.boat.interview;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -14,6 +15,14 @@ public class Main {
         // Homer: 32
         // Bart: 21
         // William: 3
+
+        Map<String,Long> firstNamesWithCount = PeopleSetup.people.stream()
+                .collect(Collectors.groupingBy(x -> x.getFirstName(),Collectors.counting()));
+
+        Map<String,Long> top3FirstNamesWithCount = firstNamesWithCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(3)
+                .collect(Collectors.toMap(x -> x.getKey(), x->x.getValue()));
     }
 
 
--- src/main/java/com/cgi/boat/interview/PeopleProcessor.java
+++ src/main/java/com/cgi/boat/interview/PeopleProcessor.java
@@ -1,7 +1,9 @@
 package com.cgi.boat.interview;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class PeopleProcessor {
     /**
@@ -18,7 +20,15 @@ class PeopleProcessor {
      * }
      */
     static Map<String, List<String>> lastnamesByFirstname(List<Person> people){
-        //TODO: implement
+        Map<String, List<String>> mapOfLastNamesByFirstName = new HashMap<>();
+        for(Person person : people) {
+            List<String> lastNamesWithRelatesFirstNames = people.stream()
+                    .filter(x -> x.getFirstName().equalsIgnoreCase(person.getFirstName()))
+                    .map(x -> x.getLastName())
+                    .collect(Collectors.toList());
+            mapOfLastNamesByFirstName.put(person.getFirstName(),lastNamesWithRelatesFirstNames);
+        }
+        return mapOfLastNamesByFirstName;
     }
 
 
@@ -35,7 +45,15 @@ class PeopleProcessor {
      *
      */
     static Map<String, List<String>> firstnamesByLastname(List<Person> people){
-        //TODO: implement
+        Map<String, List<String>> mapOfFirstNamesByLastName = new HashMap<>();
+        for(Person person : people) {
+            List<String> firstNamesWithRelatesLastNames = people.stream()
+                    .filter(x -> x.getLastName().equalsIgnoreCase(person.getLastName()))
+                    .map(x -> x.getFirstName())
+                    .collect(Collectors.toList());
+            mapOfFirstNamesByLastName.put(person.getLastName(),firstNamesWithRelatesLastNames);
+        }
+        return mapOfFirstNamesByLastName;
     }
 
 }
