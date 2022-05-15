d5044b0ba7698effa01a40c915547ba603210bad
Magyar Attila <attila.magyar@nitrowise.com>
2022-05-15 20:44:33 +0200

    solution

--- src/main/java/com/cgi/boat/interview/Main.java
+++ src/main/java/com/cgi/boat/interview/Main.java
@@ -2,6 +2,7 @@ package com.cgi.boat.interview;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -14,12 +15,24 @@ public class Main {
         // Homer: 32
         // Bart: 21
         // William: 3
+        Map<String, Long> countByFirstname = getCountByFirstname();
+        Map<String, Long> top3MostCommonFirstNameWithOccurrences = getTopFirstnames();
     }
 
+    private static Map<String, Long> getCountByFirstname() {
+        return PeopleSetup.people.stream()
+                                 .collect(Collectors.groupingBy(
+                                         Person::getFirstName,
+                                         Collectors.counting()
+                                 ));
+    }
 
-
-
-
-
+    private static Map<String, Long> getTopFirstnames() {
+        return getCountByFirstname().entrySet()
+                                    .stream()
+                                    .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                                    .limit(10)
+                                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
 }
--- src/main/java/com/cgi/boat/interview/PeopleProcessor.java
+++ src/main/java/com/cgi/boat/interview/PeopleProcessor.java
@@ -1,41 +1,55 @@
 package com.cgi.boat.interview;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class PeopleProcessor {
+
     /**
      * Returns a {@link Map} where keys are first names and values lists of all last names
      * of people from the input list who have the first name
      * equal to the key.
-     *
+     * <p>
      * Example:
      * For input: ["John Doe", "John Silver", "Peter Doe"]
      * Expected result would be:
      * {
-     *  "John" -> ["Doe", "Silver"]
-     *  "Peter" -> ["Doe"]
+     * "John" -> ["Doe", "Silver"]
+     * "Peter" -> ["Doe"]
      * }
      */
-    static Map<String, List<String>> lastnamesByFirstname(List<Person> people){
-        //TODO: implement
+    static Map<String, List<String>> lastnamesByFirstname(List<Person> people) {
+        return people.stream()
+                     .collect(Collectors.toMap(Person::getFirstName,
+                             p -> new ArrayList<>(Arrays.asList(p.getLastName())),
+                             PeopleProcessor::mergeLists));
     }
-
 
     /**
      * Same as {@link PeopleProcessor#lastnamesByFirstname} except that the mapping
      * returned is lastname -> firstnames
-     *
+     * <p>
      * Example:
      * For input: ["John Doe", "John Silver", "Peter Doe"]
      * Expected result would be:
      * {
-     *  "Doe" -> ["John", "Peter"]
-     *  "Silver" -> ["John"]
-     *
+     * "Doe" -> ["John", "Peter"]
+     * "Silver" -> ["John"]
      */
-    static Map<String, List<String>> firstnamesByLastname(List<Person> people){
-        //TODO: implement
+    static Map<String, List<String>> firstnamesByLastname(List<Person> people) {
+        return people.stream()
+                     .collect(Collectors.toMap(Person::getLastName,
+                             p -> new ArrayList<>(Arrays.asList(p.getFirstName())),
+                             PeopleProcessor::mergeLists));
+    }
+
+    private static List<String> mergeLists(List<String> baseList, List<String> newList) {
+        baseList.addAll(newList);
+        return baseList;
     }
 
 }
